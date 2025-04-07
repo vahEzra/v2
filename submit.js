51da96494899d2aa483db677ba75d92a40ce1409
vahEzra <lifecycle702@gmail.com>
2025-04-07 16:47:42 -0400

    Delete submit.js

--- submit.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const data = req.body; // Data from the client (JSON)
-    console.log(data); // Log it for now
-    return res.status(200).json({ message: 'Data received successfully', data });
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
